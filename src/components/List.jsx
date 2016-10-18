7098b716484efad2eb6e0be734fe5989f7b7b560
Pradeep Sindhav <prdeepsindhav2015@gmail.com>
2016-10-18 17:39:16 +0000

    Made the skeleton for my future projects

---
+++ src/components/List.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingrediants = [
+    {
+        "id": 1,
+        "text": "ham"
+    }, {
+        "id": 2,
+        "text": "cheese"
+    }, {
+        "id": 3,
+        "text": "potato"
+    }
+];
+
+var List = React.createClass({
+    render: function() {
+        var listItems = ingrediants.map(function(item) {
+            return <ListItem key={item.id} ingrediant={item.text}/>
+        })
+
+        return (
+            <ul>{listItems}</ul>
+        );
+    }
+})
+
+module.exports = List;
